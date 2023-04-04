73657954e91c3fef81ef8497f8ec5d5106e5a82d
Faisal <faisal.bd@asa-international.com>
2023-04-04 15:11:23 +0600

    Initial commit

---
+++ signin/src/main/java/com/example/signin/SignInFragment.kt
@@ -0,0 +1,38 @@
+package com.example.signin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.signin.databinding.FragmentSignInBinding
+
+
+class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_sign_in,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInButton.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_home_navigation)
+        }
+    }
+}
